21d70d5017190239af5718eced66d34279cf27a1
Abraham Yunes <ayunesb@icloud.com>
2025-08-21 17:57:13 -0500

    chore: add Capacitor, iOS, Android platforms and visual/contract CI upgrades

---
+++ android/app/src/main/java/escolta/services/MainActivity.java
@@ -0,0 +1,5 @@
+package escolta.services;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'escolta.services',
+  appName: 'blindado',
+  webDir: 'dist'
+};
+
+export default config;
